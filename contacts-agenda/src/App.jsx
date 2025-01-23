9fad6172478552a24f00f98e2dbd4d9aaff52e60
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-01-23 10:53:57 +0100

    Change favorite

--- contacts-agenda/src/App.jsx
+++ contacts-agenda/src/App.jsx
@@ -1,53 +1,57 @@
-import React from "react"
-import avatar from "./imgs/user.png"
-import starFilled from "./imgs/star-filled.png"
-import starEmpty from "./imgs/star-empty.png"
+import React from "react";
+import avatar from "./imgs/userDona.png";
+import filledStar from "./imgs/filled-star.png";
+import emptyStar from "./imgs/empty-star.png";
 
 export default function App() {
-    const [contact, setContact] = React.useState({
-        firstName: "Donna",
-        lastName: "Paulsen",
-        phone: "+1 (222) 847-3928",
-        email: "donnapaulsen@example.com",
-        isFavorite: true
-    })
-    
-    let starIcon = contact.isFavorite ? starFilled : starEmpty
+  const [contact, setContact] = React.useState({
+    firstName: "Donna",
+    lastName: "Paulsen",
+    phone: "+1 (222) 847-3928",
+    email: "donnapaulsen@example.com",
+    isFavorite: false
+  });
 
-    function toggleFavorite() {
-        console.log("Toggle Favorite")
-    }
-  
+  let starIcon = contact.isFavorite ? filledStar : emptyStar;
 
-    return (
-        <main>
-            <article className="card">
-                <img
-                    src={avatar}
-                    className="avatar"
-                    alt="User profile picture of John Doe"
-                />
-                <div className="info">
-                    <button
-                        onClick={toggleFavorite}
-                        aria-pressed={contact.isFavorite}
-                        aria-label={contact.isFavorite ? "Remove from favorites" : "Add to favorites"}
-                        className="favorite-button"
-                    >
-                        <img
-                            src={starIcon}
-                            alt={contact.isFavorite ? "Filled star icon" : "Empty star icon"}
-                            className="favorite"
-                        />
-                    </button>
-                    <h2 className="name">
-                        {contact.firstName} {contact.lastName}
-                    </h2>
-                    <p className="contact">{contact.phone}</p>
-                    <p className="contact">{contact.email}</p>
-                </div>
+  function toggleFavorite() {
+    setContact((prevContact) => ({
+      ...prevContact,
+      isFavorite: !prevContact.isFavorite,
+    }));
+    console.log("Toggle Favorite");
+  }
 
-            </article>
-        </main>
-    )
+  return (
+    <main>
+      <article className="card">
+        <img
+          className="avatar"
+          src={avatar}
+          alt="User profile picture of Dona Paulsen"
+        />
+        <div className="info">
+          <button
+            className="favorite-button"
+            onClick={toggleFavorite}
+            aria-pressed={contact.isFavorite}
+            aria-label={
+              contact.isFavorite ? "Remove from favorites" : "Add to favorites"
+            }
+          >
+            <img
+              className="star"
+              src={starIcon}
+              alt={contact.isFavorite ? "Filled star icon" : "Empty star icon"}
+            />
+          </button>
+          <h2 className="name">
+            {contact.firstName} {contact.lastName}
+          </h2>
+          <p className="contact">{contact.phone}</p>
+          <p className="contact">{contact.email}</p>
+        </div>
+      </article>
+    </main>
+  );
 }
